4ed2b24898263b3c71eb51f569b037d72ca9983f
Aldahir <aldahir@Aldahirs-Macbook-Pro.local>
2017-05-30 14:54:07 -0700

    Finished all Enumerable exercises!

--- lib/enumerables1.rb
+++ lib/enumerables1.rb
@@ -3,22 +3,28 @@
 # Define a method that returns an array of only the even numbers in its argument
 # (an array of integers).
 def get_evens(arr)
+  arr.map{|x| x if x.even?}.compact
 end
 
 # Define a method that returns a new array of all the elements in its argument
 # doubled. This method should *not* modify the original array.
 def calculate_doubles(arr)
+  arr.map{|x| x * 2 }
 end
 
 # Define a method that returns its argument with all the argument's elements
 # doubled. This method should modify the original array.
 def calculate_doubles!(arr)
+  arr.map!{|x| x * 2 }
 end
 
 # Define a method that returns the sum of each element in its argument
 # multiplied by its index. array_sum_with_index([2, 9, 7]) => 23 because (2 * 0) +
 # (9 * 1) + (7 * 2) = 0 + 9 + 14 = 23
 def array_sum_with_index(arr)
+  total = 0
+  arr.each_with_index{|x,y| total += (x * y) }
+  total
 end
 
 # MEDIUM
@@ -27,6 +33,9 @@ end
 # the actual retail price without going over that price. Assume there is always
 # at least one bid below the retail price.
 def price_is_right(bids, actual_retail_price)
+  total = 0
+  bids.each{|x| total += 1 if x <= actual_retail_price}
+  total
 end
 
 # Given an array of numbers, return an array of those numbers that have at least
@@ -34,22 +43,41 @@ end
 # at_least_n_factors([1, 3, 10, 16], 5) => [16] because 16 has five factors (1,
 # 2, 4, 8, 16) and the others have fewer than five factors. Consider writing a
 # helper method num_factors
-def at_least_n_factors(numbers, n)
+def num_factors(number)
+  total_factors = []
+  (1..number).each{|x| total_factors << x if number % x == 0}
+  total_factors
 end
 
-def num_factors(number)
+def at_least_n_factors(numbers, n)
+  at_least_arr = []
+  numbers.each{|x| at_least_arr << x if num_factors(x).length >= n}
+  at_least_arr
 end
+
 
 # HARD
 
 # Define a method that accepts an array of words and returns an array of those
 # words whose vowels appear in order. You may wish to write a helper method:
 # ordered_vowel_word?
-def ordered_vowel_words(words)
+def ordered_vowel_word?(word)
+  vowels = ["a", "e", "i", "o", "u"]
+  vowels_to_test = []
+  word.split("").each{|x| vowels_to_test << x if vowels.include?(x)}
+  if vowels_to_test.empty?
+    word
+  else
+    if vowels_to_test == vowels_to_test.sort
+      word
+    end
+  end
 end
 
-def ordered_vowel_word?(word)
+def ordered_vowel_words(words)
+  words.map{|x| ordered_vowel_word?(x)}.compact
 end
+
 
 # Given an array of numbers, return an array of all the products remaining when
 # each element is removed from the array. You may wish to write a helper method:
@@ -63,8 +91,15 @@ end
 # take out 1, leaving 2 * 3 * 5 15, because you take out 2, leaving 1 * 3 * 5
 # 10, because you take out 3, leaving 1 * 2 * 5 6, because you take out 5,
 # leaving 1 * 2 * 3
-def products_except_me(numbers)
+def array_product(array)
+  array.inject(:*)
 end
 
-def array_product(array)
+def products_except_me(numbers)
+  finished_array = []
+  (0...numbers.length).each{|x|
+    dummy_arr = numbers.reject{|y| y == numbers[x] }
+    finished_array << array_product(dummy_arr)
+  }
+  finished_array
 end
--- lib/enumerables2.rb
+++ lib/enumerables2.rb
@@ -5,22 +5,40 @@ require 'byebug'
 # Define a method that returns the sum of all the elements in its argument (an
 # array of numbers).
 def array_sum(arr)
+  return 0 if arr.empty?
+  arr.inject(:+)
 end
 
 # Define a method that returns a boolean indicating whether substring is a
 # substring of each string in the long_strings array.
 # Hint: you may want a sub_tring? helper method
+def sub_tring?(substring, long_string)
+  return true if long_string.include?(substring)
+  return false
+end
+
 def in_all_strings?(long_strings, substring)
+  long_strings.each{|x| return false if sub_tring?(substring, x) == false}
+  return true
 end
 
 # Define a method that accepts a string of lower case words (no punctuation) and
 # returns an array of letters that occur more than once, sorted alphabetically.
 def non_unique_letters(string)
+  new_arr = string.delete(" ").split("")
+  new_hash = Hash[new_arr.collect { |x| [x, new_arr.count(x)] }]
+  final_arr = []
+  new_hash.keys.each{|y| final_arr << y if new_hash[y] > 1 }
+  final_arr
 end
+
 
 # Define a method that returns an array of the longest two words (in order) in
 # the method's argument. Ignore punctuation!
 def longest_two_words(string)
+  new_arr = string.split(" ")
+  sorted_arr = new_arr.sort_by{|word| word.length}
+  [sorted_arr[-1],sorted_arr[-2]]
 end
 
 # MEDIUM
@@ -28,15 +46,22 @@ end
 # Define a method that takes a string of lower-case letters and returns an array
 # of all the letters that do not occur in the method's argument.
 def missing_letters(string)
+  alphabet = ("a".."z").to_a
+  new_arr = string.delete(" ").split("").uniq
+  alphabet - new_arr
 end
 
 # Define a method that accepts two years and returns an array of the years
 # within that range (inclusive) that have no repeated digits. Hint: helper
 # method?
 def no_repeat_years(first_yr, last_yr)
+  first_arr = (first_yr..last_yr).to_a
+  first_arr.map{|x| x if not_repeat_year?(x)}.compact
 end
 
 def not_repeat_year?(year)
+  new_arr = year.to_s.split("")
+  true if new_arr == new_arr.uniq
 end
 
 # HARD
@@ -48,11 +73,19 @@ end
 # weeks, they're "one-week wonders." Suggested strategy: find the songs that
 # appear multiple times in a row and remove them. You may wish to write a helper
 # method no_repeats?
-def one_week_wonders(songs)
+def no_repeats?(song_name, songs)
+  songs.each_with_index do |x,y|
+    if x == song_name
+      return false if songs[y-1] == x
+    end
+  end
+  return true
 end
 
-def no_repeats?(song_name, songs)
+def one_week_wonders(songs)
+  songs.map{|w| w if no_repeats?(w, songs)}.compact.uniq
 end
+
 
 # Define a method that, given a string of words, returns the word that has the
 # letter "c" closest to the end of it. If there's a tie, return the earlier
@@ -60,9 +93,23 @@ end
 # wish to write the helper methods c_distance and remove_punctuation.
 
 def for_cs_sake(string)
+  string_arr = string.split(" ")
+  distance_arr = string_arr.map{|x| c_distance(x)}
+  no_symbols(string_arr[distance_arr.index(distance_arr.min)])
 end
 
 def c_distance(word)
+  actual_word = no_symbols(word).reverse
+  return actual_word.index("c") unless actual_word.index("c") == nil
+  100000
+end
+
+def no_symbols(word)
+  alpha = ("a".."z").to_a
+  word_arr = word.split("")
+  new_word = ""
+  word_arr.each{|x| new_word << x if alpha.include?(x)}
+  new_word
 end
 
 # Define a method that, given an array of numbers, returns a nested array of
@@ -71,4 +118,16 @@ end
 # [[0, 1]] repeated_number_ranges([1, 2, 3, 3, 4, 4, 4]) => [[2, 3], [4, 6]]
 
 def repeated_number_ranges(arr)
+  return [] if arr == arr.uniq
+  final_arr = []
+  arr.each_with_index{|x,y|
+    count_start = y
+    count_end = y
+    while arr[count_end] == x
+      count_end += 1
+    end
+    next if final_arr.flatten.include?(count_end-1)
+    final_arr << [count_start,count_end-1] unless count_start+1 == count_end
+  }
+  final_arr
 end
